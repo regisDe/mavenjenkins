734886f0604830beb4a7e44f56f6b46ff01a2bf7
toto <toto@titi.fr>
2019-04-29 14:46:08 +0200

    initial commit

---
+++ src/main/java/fr/afcepf/al33/jenkins/Calculateur.java
@@ -0,0 +1,13 @@
+package fr.afcepf.al33.jenkins;
+
+public class Calculateur {
+	
+	public int add(int a, int b) {
+		return a + b;
+	}
+	
+	public int mult(int a, int b) {
+		return a * b;
+	}
+
+}
---
+++ src/test/java/fr/afcepf/al33/jenkins/test/CalculateurTest.java
@@ -0,0 +1,28 @@
+package fr.afcepf.al33.jenkins.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import fr.afcepf.al33.jenkins.Calculateur;
+
+public class CalculateurTest {
+	
+	private static Calculateur calcul = null;
+	
+	@BeforeAll
+	public static void init() {
+		calcul = new Calculateur();
+	}
+
+	@Test
+	public void addTest() {
+		assertEquals(42, calcul.add(2, 40), "Pb sur add");
+	}
+
+	@Test
+	public void multTest() {
+		assertEquals(42, calcul.mult(6, 7), "Pb sur mult");
+	}
+}
